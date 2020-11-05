2dfe5639e3c8c6f5b6d1a8531e3520d91760422a
christian lalanne <p21clalanne@fairfieldprep.org>
2020-11-05 13:17:20 -0500

    lab 2 and 3 done

---
+++ lab_2-1.py
@@ -0,0 +1,13 @@
+# CL 11/05/2020 
+
+print("Farfield" + " Prep") 
+
+print("-" * 20) 
+
+print("I'm loving this short story I'm reading, \"The Fall of the house of Usher\"") 
+
+previous_string = "I'm loving this short story I'm reading, \"The Fall of the House of Usher\""
+print(len(previous_string)) 
+
+my_string = "apple" 
+print(my_string[3])
---
+++ lab_3-1.py
@@ -0,0 +1,8 @@
+# Author CL 11/5/2020
+
+school = "Fairfield Prep" 
+first_half = school[:9] 
+second_half = school[9:]
+print(first_half)
+print(second_half)
+print(first_half + second_half)
